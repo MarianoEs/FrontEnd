aa51a77ebbf62df737a49b4196f9f45495b78909
MarianoEs <marianoesser@gmail.com>
2022-06-29 16:02:56 -0300

    master
    front end

--- src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -8,11 +10,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class IniciarSesionComponent implements OnInit {
   form:FormGroup;
-  constructor(private formBuilder:FormBuilder) { 
+  autenticacionService: any;
+  constructor(private formBuilder:FormBuilder, private autenticacion:AutenticacionService, private ruta:Router) { 
       this.form=this.formBuilder.group(
         {
           email:['',[Validators.required, Validators.email]],
-          password:['',[Validators.required, Validators.minLength(8)]],
+          password:['',[Validators.required, Validators.minLength(15)]],
+          deviceInfo:this.formBuilder.group({
+              notificationToken:['$2a$10$3oFWo8CHik47MbdSUo6dI.tMLZhgpy7OzVvAEZf/u500SSt/rFHim',[Validators.required, Validators.minLength(15)]]
+
+          })
         }
       )
     }
@@ -25,5 +32,13 @@ export class IniciarSesionComponent implements OnInit {
 
   get Password(){
     return this.form.get('password');
+  }
+
+  onEnviar(event:Event){
+    event.preventDefault;
+    this.autenticacionService.IniciarSesion(this.form.value).subscribe((data: any)=>{
+      console.log("DATA:" + JSON.stringify(data));
+      this.ruta.navigate(['/porfolio/']);
+    })
   }
 }
--- src/app/servicios/autenticacion.service.ts
+++ src/app/servicios/autenticacion.service.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AutenticacionService {
-  url="http://localhost:8080";
+  url="http://localhost:8080/login/";
   currentUserSubject: BehaviorSubject<any>
   constructor(private http: HttpClient) {
     console.log("el servicio esta corriendo");
